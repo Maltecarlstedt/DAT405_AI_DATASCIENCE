cb391ecbd56716b201080efd7a7fec0d0045ab09
maltecarlstedt <malte.carlstedt@hotmail.se>
2022-01-24 14:04:40 +0100

    added comments

--- LAB_1_World_In_Data/lab1.py
+++ LAB_1_World_In_Data/lab1.py
@@ -2,9 +2,17 @@ from numpy import average
 import pandas as pd 
 import matplotlib.pyplot as scatterPlot
 
+# importing from CSV files. Note the files has already been data cleaned and only has data from 2018.
+# 
 gdpCountries = pd.read_csv("gdp.csv", header = 0)
 lifeExpecCountries = pd.read_csv("life_expec.csv", header = 0)
 
+# Data cleaning the column "code" sine it has no use to us.
+del gdpCountries["Code"]
+del lifeExpecCountries["Code"]
+
+print(gdpCountries.to_string)
+print(lifeExpecCountries.to_string)
 
 matchingCountries = []
 lifeExpec = []
@@ -13,24 +21,27 @@ gdp = []
 i = 0
 j = 0
 
-# Slow code :( 
+# Slow code, there are better Pandas method for iterating over lists. 
+# This is the method that lets us find the countries that are both from the gdp csv file and the life expectancy file
 for index, gdpRows in gdpCountries.iterrows():
   for index, lifeRows in lifeExpecCountries.iterrows():
     if(gdpRows[0] == lifeRows[0]):
       matchingCountries.append(lifeRows[0])
       if(gdpRows[0] == matchingCountries[i]):
-        gdp.append(gdpRows[3])
+        gdp.append(gdpRows[2])
         i = i + 1
       if(lifeRows[0] == matchingCountries[j]):
-        lifeExpec.append(lifeRows[3])
+        lifeExpec.append(lifeRows[2])
         j = j + 1
 
+# To calculate the average for a list.
 def Average(list):
   sum = 0
   for i in range(len(list)-1):
     sum=sum+list[i]
   return sum/len(list)
 
+# To calculate the standard deviation for a list
 def Standard_dev(list):
   average = Average(list)
   deviations = []
@@ -39,17 +50,17 @@ def Standard_dev(list):
   almostdone = Average(deviations)
   return almostdone**0.5
 
-
 # print(Average(lifeExpec))
 # print(Standard_dev(lifeExpec))
-print(Average(gdp))
-print(Standard_dev(gdp))
+# print(Average(gdp))
+# print(Standard_dev(gdp))
 
+# Returns a list of all countries with one standard deviation above the average.
 def Countries_above_standard(list):
   oneAboveStandardDev = Standard_dev(list) + Average(list)
   listOfCountriesAbove = []
   for index, lifeRows in lifeExpecCountries.iterrows():
-    if(lifeRows[3] >= oneAboveStandardDev and lifeRows[0] in matchingCountries):
+    if(lifeRows[2] >= oneAboveStandardDev and lifeRows[0] in matchingCountries):
       listOfCountriesAbove.append(lifeRows[0])
   return listOfCountriesAbove
   
@@ -58,6 +69,8 @@ def Countries_above_standard(list):
 # print(len(Countries_above_standard(lifeExpec)))
 # print(len(lifeExpec))
 
+
+# Scatter our plot. The for loop is optional and is used to put labels on each data point.
 scatterPlot.scatter(gdp, lifeExpec)
 for i in range(len(matchingCountries)-1):
   scatterPlot.annotate(matchingCountries[i], (gdp[i], lifeExpec[i])) #for us to see which point represents the country
@@ -65,7 +78,7 @@ scatterPlot.ylabel("life-expectancy")
 scatterPlot.xlabel("gdp per capita")
 scatterPlot.show()
 
-
+# Debugging
 """"
 print("_____________________")
 print(matchingCountries)
