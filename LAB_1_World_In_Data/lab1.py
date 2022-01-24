058b26f73012f95ddf941faa95723876408df0e0
Johan Östling <johan.ostling123@gmail.com>
2022-01-24 14:02:50 +0100

    task 1 done

--- LAB_1_World_In_Data/lab1.py
+++ LAB_1_World_In_Data/lab1.py
@@ -1,6 +1,7 @@
 from numpy import average
 import pandas as pd 
 import matplotlib.pyplot as scatterPlot
+import math
 
 gdpCountries = pd.read_csv("gdp.csv", header = 0)
 lifeExpecCountries = pd.read_csv("life_expec.csv", header = 0)
@@ -25,6 +26,7 @@ for index, gdpRows in gdpCountries.iterrows():
         lifeExpec.append(lifeRows[3])
         j = j + 1
 
+
 def Average(list):
   sum = 0
   for i in range(len(list)-1):
@@ -37,13 +39,24 @@ def Standard_dev(list):
   for i in range(len(list)-1):
     deviations.append((list[i]-average)**2)
   almostdone = Average(deviations)
-  return almostdone**0.5
+  return math.sqrt(almostdone)
 
+def Countries_below_mean(list):
+  average = Average(list)
+  below_mean = []
+  for index, gdpRows in gdpCountries.iterrows():
+    if(gdpRows[3] < average and gdpRows[0] in matchingCountries):
+      below_mean.append(gdpRows[0])
+  return below_mean
 
-# print(Average(lifeExpec))
-# print(Standard_dev(lifeExpec))
-print(Average(gdp))
-print(Standard_dev(gdp))
+def Countries_above_mean(list):
+  average = Average(list)
+  below_mean = []
+  for index, gdpRows in gdpCountries.iterrows():
+    if(gdpRows[3] > average and gdpRows[0] in matchingCountries):
+      below_mean.append(gdpRows[0])
+  return below_mean
+
 
 def Countries_above_standard(list):
   oneAboveStandardDev = Standard_dev(list) + Average(list)
@@ -53,17 +66,24 @@ def Countries_above_standard(list):
       listOfCountriesAbove.append(lifeRows[0])
   return listOfCountriesAbove
   
+def Countries_below_standard(list):
+  oneAboveStandardDev = -Standard_dev(list) + Average(list)
+  listOfCountriesAbove = []
+  for index, lifeRows in lifeExpecCountries.iterrows():
+    if(lifeRows[3] <= oneAboveStandardDev and lifeRows[0] in matchingCountries):
+      listOfCountriesAbove.append(lifeRows[0])
+  return listOfCountriesAbove
 
-# print(Countries_above_standard(lifeExpec))
-# print(len(Countries_above_standard(lifeExpec)))
-# print(len(lifeExpec))
+print(list(set(Countries_below_standard(lifeExpec)) & set(Countries_above_mean(gdp))))
+
+#print(list(set(Countries_above_standard(lifeExpec)) & set(Countries_below_mean(gdp))))
 
 scatterPlot.scatter(gdp, lifeExpec)
 for i in range(len(matchingCountries)-1):
   scatterPlot.annotate(matchingCountries[i], (gdp[i], lifeExpec[i])) #for us to see which point represents the country
 scatterPlot.ylabel("life-expectancy")
 scatterPlot.xlabel("gdp per capita")
-scatterPlot.show()
+#scatterPlot.show()
 
 
 """"
