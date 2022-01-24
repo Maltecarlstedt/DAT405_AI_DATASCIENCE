424f00439801ce811a5fa707d1be73f97cf38367
maltecarlstedt <malte.carlstedt@hotmail.se>
2022-01-24 18:25:48 +0100

    Finished with the code

--- LAB_1_World_In_Data/lab1_2.py
+++ LAB_1_World_In_Data/lab1_2.py
@@ -2,50 +2,50 @@ import pandas as pd
 import matplotlib.pyplot as scatterPlot
 import csv
 
-
+# Reading from CSV-files
 hapiness = pd.read_csv("hapiness.csv", header = 0)
 higherEducation = pd.read_csv("higherEducation.csv", header = 0)
-savings = pd.read_csv("savings.csv", header = 0)
+trust = pd.read_csv("trust.csv", header = 0)
 
-# Data cleaning, only using the most recent data.
+# Data cleaning, only using data from the year 2019
 hapiness = hapiness[hapiness['Year']==2010]
 higherEducation = higherEducation[higherEducation['Year']==2010]
-savings = savings[savings['Year']==2010]
+# Using 2009 here since no other year was good four us.
+trust = trust[trust['Year']==2009]
 
 # Data cleaning the column "code" sine it has no use to us.
 del hapiness["Code"]
 del higherEducation["Code"]
-del savings["Code"]
-
-hapinessCountries = []
-higherEducationCountries = []
-savingsCountries = []
+del trust["Code"]
 
 # To get lists from CSV-files
 higherEducationCountries = higherEducation['Entity'].tolist()
 hapinessCountries = hapiness['Entity'].tolist()
-savingsCountries = savings['Entity'].tolist()
+trustCountries = trust['Entity'].tolist()
 
-#higherEducationData = higherEducation['higherED'].tolist()
-#hapinessData = hapiness['hapiness'].tolist()
-#savingsData = savings['savings'].tolist()
-
-
-# To get the countries that exists in 2 lists.
+# To get the countries that exists in all 3 lists.
 def getMatchingCountries(listA, listB, listC):
  matchingCountries = list(set(listA) & set(listB) & set(listC))
  return matchingCountries
 
-matchingCountries = getMatchingCountries(higherEducationCountries, hapinessCountries, savingsCountries)
+# Creating a list of all countries that exists in all files
+matchingCountries = getMatchingCountries(higherEducationCountries, hapinessCountries, trustCountries)
 
+# Removing rows that are not in matchingCountries
+hapiness = hapiness.loc[hapiness['Entity'].isin(matchingCountries)]
+trust = trust.loc[trust['Entity'].isin(matchingCountries)]
+higherEducation = higherEducation.loc[higherEducation['Entity'].isin(matchingCountries)]
 
+# Debugging
+#higherEducationCountries = higherEducation['Entity'].tolist()
+#hapinessCountries = hapiness['Entity'].tolist()
+#trustCountries = trust['Entity'].tolist()
 
-"""""
-def get_Correlated_data(listA, listB):
-  matchingCountries = getMatchingCountries(listA,listB)
-  for i in range(len(listA)):
-    for j in range(len(listB)):
-      if
-"""
-#scatterPlot.scatter(hapinessData, higherEducationData)
-#scatterPlot.show()
+# Creating list of our data columns
+higherEducationData = higherEducation['data'].tolist()
+hapinessData = hapiness['data'].tolist()
+trustData = trust['data'].tolist()
+
+# Plotting
+scatterPlot.scatter(trustData, hapinessData)
+scatterPlot.show()
