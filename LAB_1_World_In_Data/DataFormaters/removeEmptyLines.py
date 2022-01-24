8b4455d86e8a9c0c0a58eee3db22fe98bcf92323
maltecarlstedt <malte.carlstedt@hotmail.se>
2022-01-24 14:39:48 +0100

    Comments

--- LAB_1_World_In_Data/DataFormaters/removeEmptyLines.py
+++ LAB_1_World_In_Data/DataFormaters/removeEmptyLines.py
@@ -1,7 +1,12 @@
 # Found at https://stackoverflow.com/questions/4521426/delete-blank-rows-from-csv
 
 # This removes all rows that are blank in a CSV file
+# After we wrote these programs we learned that in Pandas we could use df = df[df['year']==2018] 
+# to remove the lines we didn´t need. But since we already had done it this way we continued with it. 
+# This also made it easier to handle CSV files because they became smaller.
 
 import pandas as pd
+# Reads an input file
 df = pd.read_csv('INPUTFILE.csv')
+# Creates a new file and writes to it.
 df.to_csv('OUTPUTFILE', index=False)
