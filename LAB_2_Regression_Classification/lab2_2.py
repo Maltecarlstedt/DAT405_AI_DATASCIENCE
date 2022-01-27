915e44123394c3585257f13b842a3058fd327e06
Johan Östling <johan.ostling123@gmail.com>
2022-01-27 10:42:27 +0100

    not done with correct labels confusion matrix

--- LAB_2_Regression_Classification/lab2_2.py
+++ LAB_2_Regression_Classification/lab2_2.py
@@ -12,7 +12,9 @@ from sklearn import metrics
 
 # Dataset from sklearn
 iris = datasets.load_iris()
+class_names = iris.target_names
 
+#split the data to 25% test set and 75% training set
 x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.25, random_state=0)
 
 
@@ -22,20 +24,24 @@ logisticRegr = LogisticRegression(multi_class='ovr', solver='liblinear')
 # Train the module 
 logisticRegr.fit(x_train, y_train)
 
-logisticRegr.predict(x_test[0].reshape(1,-1))
-logisticRegr.predict(x_test[0:10])
+#gives predicted flower from test values and returns as a numpy array
+#logisticRegr.predict(x_test[0:len(x_test)-1]).reshape(1,-1)
 
+#gives predicted flower from test values but does not return as numpy array
 predictions = logisticRegr.predict(x_test)
+
+#gives a percentage of how many prediction was correct
 score = logisticRegr.score(x_test, y_test)
 print(score)
 
+
 cm = metrics.confusion_matrix(y_test, predictions)
 
-plt.figure(figsize=(3,3))
-sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
-plt.ylabel('Actual label');
-plt.xlabel('Predicted label');
+#plotting the confusion matrix
+sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r')
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
 all_sample_title = 'Accuracy Score: {0}'.format(score)
-plt.title(all_sample_title, size = 15);
+plt.title(all_sample_title, size = 15)
 # plt.savefig('toy_Digits_ConfusionSeabornCodementor.png') Saves it as a .png wtf lmao
-plt.show();
+plt.show()
