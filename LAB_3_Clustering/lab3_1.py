b3cb1fd4e23458f0bd7d54615896f34258797456
Johan Östling <johan.ostling123@gmail.com>
2022-02-02 11:27:53 +0100

    scatter plot done

--- LAB_3_Clustering/lab3_1.py
+++ LAB_3_Clustering/lab3_1.py
@@ -1,1 +1,18 @@
-print("hello world")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#save all data in file data_200.csv except the first row
+data200 = pd.read_csv("data_200.csv", header = 0)
+
+#saves columns that we want to scatter plot in lists
+phi = data200['phi'].tolist()
+psi = data200['psi'].tolist()
+
+
+#plotting and adding titles
+plt.title("Phi and psi combinations")
+plt.ylabel("psi")
+plt.xlabel("phi")
+plt.scatter(phi,psi)
+plt.show()
