c4bf958041516ee04506d461523dc83bf2b7f408
Johan Östling <johan.ostling123@gmail.com>
2022-02-02 13:55:58 +0100

    centers done

--- LAB_3_Clustering/lab3_1.py
+++ LAB_3_Clustering/lab3_1.py
@@ -1,18 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+import numpy as np
 
 #save all data in file data_200.csv except the first row
 data200 = pd.read_csv("data_200.csv", header = 0)
 
-#saves columns that we want to scatter plot in lists
-phi = data200['phi'].tolist()
-psi = data200['psi'].tolist()
+#delete columns we dont want
+del data200['residue name']
+del data200['position']
+del data200['chain']
 
+#turn data200 into a 2d numpy array
+arr_2d = data200.to_numpy()
 
+#x = phi, y = psi
+plt.scatter(arr_2d[:,0],arr_2d[:,1])
+
+#from the scatter plot we see that the points form 4 clusters
+kmeans = KMeans(n_clusters=4, random_state=0)
+
+#fits the centers to 4 clusters in our data set
+kmeans.fit(arr_2d)
+
+#plots the centers
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', marker='x')
+plt.show()
+
+"""
 #plotting and adding titles
 plt.title("Phi and psi combinations")
 plt.ylabel("psi")
 plt.xlabel("phi")
 plt.scatter(phi,psi)
 plt.show()
+"""
