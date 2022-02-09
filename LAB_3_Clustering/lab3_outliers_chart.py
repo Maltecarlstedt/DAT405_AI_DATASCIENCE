fb18fe742930f3dce0b6d9cd3fdfb4f41b9616de
maltecarlstedt <malte.carlstedt@hotmail.se>
2022-02-09 14:40:07 +0100

    More comments

--- LAB_3_Clustering/lab3_outliers_chart.py
+++ LAB_3_Clustering/lab3_outliers_chart.py
@@ -1,25 +1,25 @@
+__author__ = "Malte Carlstedt, Johan Östling"
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Read data from csv-file
 data = pd.read_csv("data_all.csv", header = 0)
 
-#delete columns we dont want so we can create 2d array later
-
-
-#turn data200 into a 2d numpy array
+# turn data into a 2d numpy array
 X = data[["phi", "psi"]].to_numpy()
 
-
+# Computing and fitting using DBSCAN.
 db = DBSCAN(eps=12, metric="euclidean", min_samples=70).fit(X)
 
+# Retrieving all noisepoints in a list (removing the datapoints that are not considered outliers)
 noisePoints = data[db.labels_==-1]
 
 # Styling for easier look
 plt.style.use('ggplot')
 plt.title("Bargraph for how many noise point relates to each residue")
+# Plotting each aminoacids depending on how many time a certain residue is in the noisePoints list. Plotting as as a barchart with colouring for easier look.
 aminoAcids = noisePoints["residueName"].value_counts().plot(kind = "bar", color = ["green","red"])
-
 
 plt.show()
--- LAB_3_Clustering/lab3_silhouette_score.py
+++ LAB_3_Clustering/lab3_silhouette_score.py
@@ -1,9 +1,10 @@
+__author__ = "Malte Carlstedt, Johan Östling"
 import math
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
-#save all data in file data_200.csv except the first row
+# Read data from Csv-file
 allData = pd.read_csv("data_all.csv", header = 0)
 
 #delete columns we dont want so we can create 2d array later
@@ -11,31 +12,24 @@ del allData['residue name']
 del allData['position']
 del allData['chain']
 
-# Convert to simple lists that is used to plot the clusters in the first assignment.
-phi = allData['phi'].tolist()
-psi = allData['psi'].tolist()
-
-#turn data200 into a 2d numpy array
+#turn data2 into a 2d numpy array
 arr_2d = allData.to_numpy()
 
-#taking the cosine function of every data point
+#taking the cosine function of every data point, used for assignment 2D
 #for i in range(len(arr_2d[:,0]-1)):
 #    arr_2d[i,0]=math.cos(math.radians(arr_2d[i,0]))
 #    arr_2d[i,1]=math.cos(math.radians(arr_2d[i,1]))
 
- 
+# For loop for trying out different values of K clusters.
 for k in range(2,10):
-  clusterer = KMeans(n_clusters=k, random_state=0)
-  cluster_labels = clusterer.fit_predict(arr_2d)
-
-  # The silhouette_score gives the average value for all the samples.
-  # This gives a perspective into the density and separation of the formed
-  # clusters
-  silhouette_avg = silhouette_score(arr_2d, cluster_labels)
+  clusters = KMeans(n_clusters=k, random_state=0)
+  clusterLabeler = clusters.fit_predict(arr_2d)
+  # Calculating the sillhoutte-score to find the average of each sample to find the density
+  avgSillhouette = silhouette_score(arr_2d, clusterLabeler)
   print(
-        "For k-clusters =",
+        "For k number of clusters =",
         k,
-        "The average silhouette score is then :",
-        silhouette_avg,
+        "Average silhouette score is:",
+        avgSillhouette,
     )
 
