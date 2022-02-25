c5befc4446954ba7d98abe87882f0cbbf5a550af
Johan Östling <johan.ostling123@gmail.com>
2022-02-25 14:05:36 +0100

    import done

---
+++ LAB_6_Neural_Networks/lab_6_q1-2.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+from tensorflow import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+
