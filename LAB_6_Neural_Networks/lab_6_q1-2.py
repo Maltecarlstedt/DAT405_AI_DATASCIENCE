cbd1af3e30f719a74ac17d1139dee7309bec41e1
Johan Östling <johan.ostling123@gmail.com>
2022-02-28 09:32:56 +0100

    adding code

--- LAB_6_Neural_Networks/lab_6_q1-2.py
+++ LAB_6_Neural_Networks/lab_6_q1-2.py
@@ -6,4 +6,53 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
+#hyper-parameters data-loading and formatting
 
+batch_size = 128
+num_classes = 10
+epochs = 10
+
+img_rows, img_cols = 28, 28
+
+(x_train, lbl_train), (x_test, lbl_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+#preprocessing
+
+x_train = x_train.astype('float32') 
+x_test = x_test.astype('float32')
+
+x_train /= 255
+x_test /= 255
+
+y_train = keras.utils.to_categorical(lbl_train, num_classes)
+y_test = keras.utils.to_categorical(lbl_test, num_classes)
+
+## Define model ##
+model = Sequential()
+
+model.add(Flatten())
+model.add(Dense(64, activation = 'relu'))
+model.add(Dense(64, activation = 'relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+               optimizer=keras.optimizers.SGD(lr = 0.1),
+        metrics=['accuracy'],)
+
+fit_info = model.fit(x_train, y_train,
+           batch_size=batch_size,
+           epochs=epochs,
+           verbose=1,
+           validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss: {}, Test accuracy {}'.format(score[0], score[1]))
