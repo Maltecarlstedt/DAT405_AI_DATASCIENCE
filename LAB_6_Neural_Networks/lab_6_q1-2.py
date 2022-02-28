4714434f7fb9e09feb5f77f75a8af186908b41db
maltecarlstedt <malte.carlstedt@hotmail.se>
2022-02-28 11:29:00 +0100

    Answer to 2 c

--- LAB_6_Neural_Networks/lab_6_q1-2.py
+++ LAB_6_Neural_Networks/lab_6_q1-2.py
@@ -5,6 +5,8 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
+import matplotlib.pyplot as plt
+import numpy as np
 
 #hyper-parameters data-loading and formatting
 
@@ -56,3 +58,25 @@ fit_info = model.fit(x_train, y_train,
            validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss: {}, Test accuracy {}'.format(score[0], score[1]))
+
+"""
+# For printing accuracy
+x = np.arange(epochs)
+y = np.array(fit_info.history["accuracy"])
+
+plt.title("Plot for accuracy")
+plt.ylabel("Accuracy in percentage %")
+plt.xlabel("Epochs")
+plt.plot(x,y)
+plt.show()
+
+"""
+# For plotting validation accuracy.
+plt.title("Plot for validation accuracy")
+x = np.arange(epochs)
+y = np.array(fit_info.history["val_accuracy"])
+
+plt.ylabel("Validation accuracy in percentage %")
+plt.xlabel("Epochs")
+plt.plot(x,y)
+plt.show()
